2b733b9070d265e13b80b95a1d0bddd81a282734
Anton <ermakov.ant2018@yandex.ru>
2023-08-07 16:07:52 +0300

    redesigned the toolbar component, added svgselector component, deleted unnecessary files

--- src/components/toolbar/ToolBar.tsx
+++ src/components/toolbar/ToolBar.tsx
@@ -1,46 +1,35 @@
 import style from "../../styles/ToolBar.module.css";
-import erase from "../../icons/eraser 1.svg";
-import brush from "../../icons/paintbrush 1.svg";
-import circle from "../../icons/circle.svg";
-import line from "../../icons/line.svg";
-import rect from "../../icons/rect.svg";
-import redo from "../../icons/redo-arrow-symbol 1.svg";
-import undo from "../../icons/undo-circular-arrow 1.svg";
-import diskette from "../../icons/diskette 1.svg";
+import SvgSelector from "../svgselector/SvgSelector";
+
+import { tools, functions } from "../../utils/toolNames";
+import { useState } from "react";
+
 const Toolbar = () => {
+  const [activeIcon, setActiveIcon] = useState<string>("brush");
+
   return (
     <div className={style.toolbar}>
       <div className={style.toolbar_top}>
-        <div>
-          <img src={brush} alt="brush" />
-        </div>
-
-        <div>
-          <img src={line} alt="line" />
-        </div>
-
-        <div>
-          <img src={rect} alt="rect" />
-        </div>
-
-        <div>
-          <img src={circle} alt="circle" />
-        </div>
-
-        <div>
-          <img src={erase} alt="erase" />
-        </div>
+        {tools.map((item) => (
+          <div
+            className={activeIcon === item ? style.activeSvg : style.svg}
+            key={item}
+            onClick={() => setActiveIcon(item)}
+          >
+            <SvgSelector id={item} />
+          </div>
+        ))}
       </div>
       <div className={style.toolbar_bot}>
-        <div>
-          <img src={undo} alt="undo" />
-        </div>
-        <div>
-          <img src={redo} alt="redo" />
-        </div>
-        <div>
-          <img src={diskette} alt="diskette" />
-        </div>
+        {functions.map((item) => (
+          <div
+            key={item}
+            onClick={() => setActiveIcon(item)}
+            className={activeIcon === item ? style.activeSvg : style.svg}
+          >
+            <SvgSelector id={item} />
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/utils/toolNames.tsx
@@ -0,0 +1,2 @@
+export const tools: string[] = ["brush", "line", "rect", "circle", "erase"];
+export const functions: string[] = ["undo", "redo", "diskette"];
