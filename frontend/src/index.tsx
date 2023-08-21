5209f9c5e7ab19d6d7923604941e20f77a2693be
Anton <ermakov.ant2018@yandex.ru>
2023-08-21 19:17:20 +0300

    added basic ws server, react router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,30 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: `/:id`,
+    element: <App />,
+  },
+  {
+    path: "*",
+    element: <Navigate to={`f${(+new Date()).toString(16)}`} replace />,
+  },
+]);
+
 root.render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 );
+//
